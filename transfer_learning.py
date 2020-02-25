8df5229a7ad85e9670ecb2186fcf04385b0156e7
Jean Merlet <jean.joseph.merlet@gmail.com>
2020-02-25 14:45:37 -0500

    add some notation / clarity to transfer_learning.py

--- transfer_learning.py
+++ transfer_learning.py
@@ -38,35 +38,37 @@ for x in ['train', 'val']:
                                                batch_size=4,
                                                shuffle=True,
                                                num_workers=4)
+  #print(image_datasets['train'])
   dataset_sizes[x] = len(image_datasets[x])
+  #print(dataset_sizes)
 
 class_names = image_datasets['train'].classes
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def imshow(inp, title=None):
-  inp = inp.numpy().transpose((1, 2, 0))
+def show_sample_img_batch(img_batch, title=None):
+  img_batch = img_batch.numpy().transpose((1, 2, 0))
   mean = np.array([0.485, 0.456, 0.406])
   std = np.array([0.229, 0.224, 0.225])
-  inp = std * inp + mean
-  inp = np.clip(inp, 0, 1)
-  plt.imshow(inp)
+  img_batch = std * img_batch + mean
+  img_batch = np.clip(img_batch, 0, 1)
+  plt.imshow(img_batch)
   if title is not None:
     plt.title(title)
   plt.pause(0.001)
 
-inputs, classes = next(iter(dataloaders['train']))
-out = torchvision.utils.make_grid(inputs)
-imshow(out, title=[class_names[x] for x in classes])
+img_batch_tensors, classes = next(iter(dataloaders['train']))
+img_batch_grid = torchvision.utils.make_grid(img_batch_tensors)
+show_sample_img_batch(img_batch_grid, title=[class_names[x] for x in classes])
 input()
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-  since = time.time()
+def train_model(model, criterion, optimizer, scheduler, num_epochs):
+  start_time = time.time()
 
-  best_model_wts = copy.deepcopy(model.state_dict())
+  best_model_weights = copy.deepcopy(model.state_dict())
   best_acc = 0.0
 
   for epoch in range(num_epochs):
-    print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+    print('Epoch {}/{}'.format(epoch + 1, num_epochs))
     print('-' * 10)
 
     for phase in ['train', 'val']:
@@ -106,17 +108,17 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
       if phase == 'val' and epoch_acc > best_acc:
         best_acc = epoch_acc
-        best_model_wts = copy.deepcopy(model.state_dict())
+        best_model_weights = copy.deepcopy(model.state_dict())
 
     print()
 
-  time_elapsed = time.time() - since
+  time_elapsed = time.time() - start_time
   print('Training complete in {:.0f}m {:.0f}s'.format(
       time_elapsed // 60, time_elapsed % 60))
   print('Best val Acc: {:4f}'.format(best_acc))
 
   # load best model weights
-  model.load_state_dict(best_model_wts)
+  model.load_state_dict(best_model_weights)
   return model
 
 def visualize_model(model, num_images=6):
@@ -145,7 +147,9 @@ def visualize_model(model, num_images=6):
                     return
         model.train(mode=was_training)
 
+# Load a fully trained model
 model_ft = models.resnet18(pretrained=True)
+# Get number of input features of last fully connected layer
 num_ftrs = model_ft.fc.in_features
 # Here the size of each output sample is set to 2.
 # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
@@ -161,12 +165,12 @@ optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
-num_epochs = 3
+num_epochs = 7
 model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs)
 
 model_savepath = './data/models/'
 model_name = str(num_epochs) + '_epochs.pt'
+torch.save(model_ft, model_savepath + model_name)
 
 visualize_model(model_ft)
 input()
-torch.save(model_ft, model_savepath + model_name)
